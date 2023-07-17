17e3df9c2727316b18e906c5f90a786c33e2a189
Panapol <panapolwin@gmail.com>
2023-07-17 16:40:57 +0700

    XO

---
+++ XO.js
@@ -0,0 +1,91 @@
+let buttons = document.querySelectorAll(".xoimg")
+let worl = document.querySelector("#worl")
+let r = 0
+let count = [buttons.length]
+let score = [buttons.length]
+
+function select(a){
+    if(r%2 == 0 && count[a] != 1){
+        buttons[a].src = "img/o.png"
+        score[a] = 'o'
+        tudsin()
+        r+=1
+    }
+    else if(r%2 != 0 && count[a] != 1){
+        buttons[a].src = "img/x.png"
+        score[a] = 'x'
+        tudsin()
+        r+=1
+    }
+    count[a] = 1
+}
+function tudsin(){
+    if(score[0]=='o' && score[3]=='o' && score[6]=='o'){
+        worl.textContent = `O Win!`
+        worl.style.color = "deepskyblue"
+    }
+    if(score[1]=='o' && score[4]=='o' && score[7]=='o'){
+        worl.textContent = `O Win!`
+        worl.style.color = "deepskyblue"
+    }
+    if(score[2]=='o' && score[5]=='o' && score[8]=='o'){
+        worl.textContent = `O Win!`
+        worl.style.color = "deepskyblue"
+    }
+    if(score[0]=='o' && score[1]=='o' && score[2]=='o'){
+        worl.textContent = `O Win!`
+        worl.style.color = "deepskyblue"
+    }
+    if(score[3]=='o' && score[4]=='o' && score[5]=='o'){
+        worl.textContent = `O Win!`
+        worl.style.color = "deepskyblue"
+    }
+    if(score[6]=='o' && score[7]=='o' && score[8]=='o'){
+        worl.textContent = `O Win!`
+        worl.style.color = "deepskyblue"
+    }
+    if(score[0]=='o' && score[4]=='o' && score[8]=='o'){
+        worl.textContent = `O Win!`
+        worl.style.color = "deepskyblue"
+    }
+    if(score[2]=='o' && score[4]=='o' && score[6]=='o'){
+        worl.textContent = `O Win!`
+        worl.style.color = "deepskyblue"
+    }
+    if(score[0]=='x' && score[3]=='x' && score[6]=='x'){
+        worl.textContent = `X Win!`
+        worl.style.color = "red"
+    }
+    if(score[1]=='x' && score[4]=='x' && score[7]=='x'){
+        worl.textContent = `X Win!`
+        worl.style.color = "red"
+    }
+    if(score[2]=='x' && score[5]=='x' && score[8]=='x'){
+        worl.textContent = `X Win!`
+        worl.style.color = "red"
+    }
+    if(score[0]=='x' && score[1]=='x' && score[2]=='x'){
+        worl.textContent = `X Win!`
+        worl.style.color = "red"
+    }
+    if(score[3]=='x' && score[4]=='x' && score[5]=='x'){
+        worl.textContent = `X Win!`
+        worl.style.color = "red"
+    }
+    if(score[6]=='x' && score[7]=='x' && score[8]=='x'){
+        worl.textContent = `X Win!`
+        worl.style.color = "red"
+    }
+    if(score[0]=='x' && score[4]=='x' && score[8]=='x'){
+        worl.textContent = `X Win!`
+        worl.style.color = "red"
+    }
+    if(score[2]=='x' && score[4]=='x' && score[6]=='x'){
+        worl.textContent = `X Win!`
+        worl.style.color = "red"
+    }
+}
+
+for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener("click", ()=>select(i))
+}
